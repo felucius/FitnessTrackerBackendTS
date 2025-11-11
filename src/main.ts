27043a6c64ac82d9757e06397e37f7b36dc3b514
felucius <maxime.de.lange@outlook.com>
2025-11-11 09:51:27 +0100

    Added dotnev env in the main ts, to make use of the prisma config settings. Also added the use of json via express and app router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import router from './routes/index';
+import express from 'express';
+
+console.log('Backend Application Starting...');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(express.json());
+
+  app.use('/', router);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
