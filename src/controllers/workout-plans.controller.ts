f64aa026c75467f882d151b9132d4e6527e249ba
felucius <maxime.de.lange@outlook.com>
2025-11-11 11:52:42 +0100

    Added workout-plans.controller.ts. WIP: Currently able to retrieve all and specific workoutplans (with correct user, exercises and progressions). Also did a name change for controllers

---
+++ src/controllers/workout-plans.controller.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import express from 'express';
+import { PrismaClient } from '../../generated/prisma/client';
+import { WorkoutPlanDetailedResponse } from '../dto/workout.plans.dto';
+import { asNonNullString, normalizeStringArray } from '../helpers/helper';
+
+class WorkoutPlansController {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    GetAllWorkoutPlans = async (request: express.Request, response: express.Response) => {
+        try {
+            const workoutPlans = await this.prisma.workoutPlans.findMany();
+            return response.status(200).json({ data: workoutPlans });
+        } catch (error) {
+            return response.sendStatus(400);
+        }
+    }
+
+    GetWorkoutPlanById = async (request: express.Request, response: express.Response) => {
+        try {
+            const {id} = request.params;
+
+            // Fetch workout plan including User and Exercises + Progressions
+            const workoutplan = await this.prisma.workoutPlans.findFirst({
+                where: { Id: id },
+                include: {
+                    Users: true,
+                    Exercises: {
+                        include: {
+                            Progression: {
+                                orderBy: { Date: 'desc' },
+                            },
+                        },
+                    },
+                },
+            });
+
+            if (!workoutplan) {
+                return response.sendStatus(404);
+            }
+
+            // Map Prisma object into your DTO
+            const dto: WorkoutPlanDetailedResponse = {
+                Id: workoutplan.Id,
+                UserId: workoutplan.UserId,
+                User: workoutplan.Users,
+                Name: asNonNullString(workoutplan.Name),
+                Type: workoutplan.Type,
+                Description: workoutplan.Description,
+                Frequency: workoutplan.Frequency,
+                Exercises: (workoutplan.Exercises ?? []).map((e) => ({
+                    UniqueExerciseId: e.UniqueExerciseId,
+                    ExerciseId: e.ExerciseId,
+                    WorkoutPlanId: e.WorkoutPlanId,
+                    Name: asNonNullString(e.Name),
+                    ImageUrl: e.ImageUrl,
+                    ExerciseType: e.ExerciseType,
+                    TargetMuscles: normalizeStringArray(e.TargetMuscles),
+                    BodyParts: normalizeStringArray(e.BodyParts),
+                    Progressions: (e.Progression ?? [])
+                        .filter((p) => p.UserId === workoutplan.UserId)
+                        .map((p) => ({
+                        Id: p.Id,
+                        UserId: p.UserId,
+                        UniqueExerciseId: p.UniqueExerciseId,
+                        ExerciseId: e.ExerciseId,
+                        Exercise: null,
+                        Date: p.Date,
+                        Weight: p.Weight,
+                        Reps: p.Reps,
+                    })),
+                })),
+            };
+
+            // Return the strongly typed DTO
+            return response.status(200).json({ data: dto });
+        } catch (error) {
+            console.error('GetWorkoutPlanById error:', error);
+            return response.sendStatus(400);
+        }
+    }
+}
+
+export default new WorkoutPlansController();
