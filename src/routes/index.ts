2104817bb061f2f15b474f3347581ac9ab7f0403
felucius <maxime.de.lange@outlook.com>
2025-11-11 11:53:47 +0100

    Added extra routes for workout plans. WIP: currently able to retrieve all and workoutplans by id

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
-import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/users.controller';
+import WorkoutPlansController from '../controllers/workout-plans.controller';
 
 const router = express.Router();
 
+// Users routes
 router.get('/users', UsersController.getAllUsers);
 router.get('/users/:id', UsersController.getUserById);
 router.post('/users', UsersController.createUser);
 router.put('/users/:id', UsersController.updateUser);
 router.delete('/users/:id', UsersController.deleteUser);
+
+// Workout Plans routes
+router.get('/workoutPlans', WorkoutPlansController.GetAllWorkoutPlans);
+router.get('/workoutPlans/:id', WorkoutPlansController.GetWorkoutPlanById);
 
 export default router;
