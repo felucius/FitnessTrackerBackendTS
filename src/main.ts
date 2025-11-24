1d41d9d843fe23d1ca7938c4e9460395fabcf7c5
felucius <maxime.de.lange@outlook.com>
2025-11-24 10:35:37 +0100

    Added CORS policy to due cross origin browser

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import router from './routes/index';
 import express from 'express';
-
+import cors from 'cors';
 console.log('Backend Application Starting...');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
 
   app.use(express.json());
 
