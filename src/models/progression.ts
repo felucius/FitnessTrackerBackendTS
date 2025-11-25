7c836d45baadb9d402c5762a8008c975c4953c3a
felucius <maxime.de.lange@outlook.com>
2025-11-25 13:52:42 +0100

    Added exercise property to progression class

--- src/models/progression.ts
+++ src/models/progression.ts
@@ -1,13 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { User } from './user';
 import { Exercise } from './exercise';
 
 export type Progression = {
     Id: string;
     UserId: string;
-    User: User;
-    ExerciseId: string;
     UniqueExerciseId: string;
+    ExerciseId: string;
     Exercise: Exercise;
     Date: Date;
     Weight: number;
